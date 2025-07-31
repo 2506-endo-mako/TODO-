e85482a95d3184aa1fa815a7aee576052676f76d
Atsumi_Yuka <atsumi.yuka@alhinc.jp>
2025-07-31 13:04:45 +0900

    TODO TOP画面実装途中

--- src/main/java/com/example/spring_boot/controller/TasksController.java
+++ src/main/java/com/example/spring_boot/controller/TasksController.java
@@ -1,6 +1,33 @@
 package com.example.spring_boot.controller;
 
+import com.example.spring_boot.controller.form.TasksForm;
+import com.example.spring_boot.service.TasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
 public class TasksController {
+    @Autowired
+    TasksService tasksService;
+
+    /*
+     * TOP画面表示処理
+     */
+    @GetMapping
+    public ModelAndView top() {
+        ModelAndView mav = new ModelAndView();
+        // タスクを全件取得
+        List<TasksForm> contentData = tasksService.findAllTasks();
+        // 画面遷移先を指定
+        mav.setViewName("/top");
+        // 投稿データオブジェクトを保管
+        mav.addObject("contents", contentData);
+        return mav;
+    }
 
 
 }
--- src/main/java/com/example/spring_boot/repository/TasksRepository.java
+++ src/main/java/com/example/spring_boot/repository/TasksRepository.java
@@ -1,4 +1,12 @@
 package com.example.spring_boot.repository;
 
-public class TasksRepository {
+import com.example.spring_boot.repository.entity.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Tasks, Integer> {
+    public List<Tasks> findAllByOrderByLimitDateAsc();
 }
