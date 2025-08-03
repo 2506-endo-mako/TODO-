e4312fbb807ac8d59494ad4b1370817b716fa060
Atsumi_Yuka <atsumi.yuka@alhinc.jp>
2025-08-04 08:00:30 +0900

    top画面レイアウト修正

--- src/main/java/com/example/spring_boot/controller/TasksController.java
+++ src/main/java/com/example/spring_boot/controller/TasksController.java
@@ -2,6 +2,7 @@ package com.example.spring_boot.controller;
 
 import com.example.spring_boot.controller.form.TasksForm;
 import com.example.spring_boot.service.TasksService;
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,10 +48,11 @@ public class TasksController {
         mav.addObject("endDate", endDate);
         mav.addObject("content", content);
         mav.addObject("status", status);
-        mav.addObject("date", LocalDate.now());
 
-        //contentDataからlimitDateを取得
-        //BeforeとAfterに分けてビューに渡す
+        //今日の日付をString型に変換
+        LocalDate today = LocalDate.now();
+        String todayStr = today.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        mav.addObject("today", todayStr);
 
         return mav;
     }
