797e26eb3701057e57c7258ad376362c8e886407
endo_mako <endo.mako@alhinc.jp>
2025-08-05 10:51:51 +0900

    ラスト

--- src/main/java/com/example/spring_boot/repository/TasksRepository.java
+++ src/main/java/com/example/spring_boot/repository/TasksRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<Tasks, Integer> {
-    public List<Tasks> findByLimitDateBetweenAndContentAndStatusOrderByLimitDateAsc(Date start,Date end ,String content, Integer status);
-    public List<Tasks> findByLimitDateBetweenAndStatusOrderByLimitDateAsc(Date start,Date end,Integer status);
-    public List<Tasks> findByLimitDateBetweenAndContentOrderByLimitDateAsc(Date start,Date end,String content);
-    public List<Tasks> findByLimitDateBetweenOrderByLimitDateAsc(Date start,Date end);
+    public List<Tasks> findByLimitDateBetweenAndContentAndStatusOrderByLimitDateAsc(Date start, Date end, String content, Integer status);
+    public List<Tasks> findByLimitDateBetweenAndStatusOrderByLimitDateAsc(Date start, Date end, Integer status);
+    public List<Tasks> findByLimitDateBetweenAndContentOrderByLimitDateAsc(Date start, Date end, String content);
+    public List<Tasks> findByLimitDateBetweenOrderByLimitDateAsc(Date start, Date end);
 }
